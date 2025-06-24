daa1593b8c3d64bbe6f0e7968445c4670512122a
kishore-syren <kishore.r@syrencloud.com>
2025-06-24 21:54:06 +0530

    App.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
- import React from 'react';
+import React from 'react';
 
 function App() {
   return (
     <div>
-      <h1>Hello, Github pages!</h1>
+      <h1>Hello, GitHub Pages!</h1>
       <p>This is a sample React app deployed with GitHub Pages.</p>
+      <p><strong>— by Kishore</strong></p> 
     </div>
   );
 }
 
 export default App;
-
