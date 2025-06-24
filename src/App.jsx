e245246ee424275ebddc6edc38dae88550503f68
kishore-syren <kishore.r@syrencloud.com>
2025-06-25 01:50:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello, Github pages!</h1>
+      <h1>Hello, Github page!</h1>
       <p>This is a sample React app deployed with GitHub Pages.</p>
     </div>
   );
